eddd629d75e824b98faa59f50b05727210f70bc6
Cristiano Faustino <cristiadu@gmail.com>
2014-02-06 08:24:47 +0000

    changing the language

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+			<link rel="stylesheet" href="css/bootstrap.min.css">
+			<link rel="stylesheet" href="css/style.css">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+			
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-1 col-md-5"></div>
+				<div class="initial-page col-xs-12 col-md-5">
+					<div class="div-space-initial"></div>
+					<div align="center">
+						<img class="img-ucsd" src="img/ucsd-logo.gif"></img>
+					</div>
+					<div class="div-space-initial"></div>
+					<div class="div-line-initial"></div>
+				</div>
+				<div class="col-xs-1 col-md-3"></div>
+			</div>
+		</div>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
