1fc00334bd5e4c5e2994cb367e26911d361ab8f6
Cristiano Faustino <cristiadu@gmail.com>
2014-02-07 06:17:08 +0000

    put the navigate page and the navigation to the other ones

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,26 @@ $(document).ready(function(){
 		event.preventDefault();
 		$(location).attr('href',"menu.html");
 	});
+
+	$("#btn-nav").click(function(){
+		event.preventDefault();
+		$(location).attr('href',"navigate.html");
+	});
+
+	$("#btn-dict").click(function(){
+		event.preventDefault();
+		$(location).attr('href',"dictionary.html");
+	});
+
+	$("#btn-plan").click(function(){
+		event.preventDefault();
+		$(location).attr('href',"plan.html");
+	});
+
+	$("#btn-con").click(function(){
+		event.preventDefault();
+		$(location).attr('href',"connect.html");
+	});
+
+
 });
