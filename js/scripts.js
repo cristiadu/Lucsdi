bdda66c9239a313420e1f328160c8afc46559a91
Cristiano Faustino <cristiadu@gmail.com>
2014-02-07 06:56:10 +0000

    add dictionary page

--- js/scripts.js
+++ js/scripts.js
@@ -25,5 +25,21 @@ $(document).ready(function(){
 		$(location).attr('href',"connect.html");
 	});
 
+	$("#search-dict").click(function(){
+		event.preventDefault();
+		$("#content-dict").html("<h2>Something</h2>"
+							+"<small>some-thing  [suhm-thing]</small>"
+							+"<p><strong>pronoun</strong></p>"
+							+"<p>1. some thing; a certain undetermined or unspecified thing: Something is wrong there. Something's happening.</p>"
+							+"<p>2. an additional amount, as of cents or minutes, that is unknown, unspecified, or forgotten: He charged me ten something for the hat. Our train gets in at two something.</p>"
+							+"<p><strong>noun</strong></p>"
+							+"<p>3.Informal. a person or thing of some value or consequence: He is really something! This writer has something to say and she says it well.</p>"
+							+"<p><strong>adverb</strong></p>"
+							+"<p>4.in some degree; to some extent; somewhat.</p>"
+							+"<p>5.Informal. to a high or extreme degree; quite: He took on something fierce about my tardiness.</p>");
+	});
+
+	
+
 
 });
