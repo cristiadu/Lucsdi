0336bc43f4f63904518f28233690aecd86602bbe
Cristiano Faustino <cristiadu@gmail.com>
2014-02-12 04:00:32 +0000

    Fix the height problem

--- index.php
+++ index.php
@@ -7,16 +7,17 @@
 	</head>
 			
 	<body>
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-1 col-md-3"></div>
-				<div class="initial-page col-xs-12 col-md-5">
+		<div id="wrap">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-1 col-md-3"></div>
+					<div class="initial-page col-xs-12 col-md-5">
 
+					</div>
+					<div class="col-xs-1 col-md-3"></div>
 				</div>
-				<div class="col-xs-1 col-md-3"></div>
 			</div>
 		</div>
-		
 		<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
 		<script type="text/javascript" src="js/bootstrap.min.js"></script>	
 		<script type="text/javascript" src="js/scripts.js"></script>
