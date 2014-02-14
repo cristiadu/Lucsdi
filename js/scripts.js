a939544dfeea5a472e89bccb243d930996911a0b
Cristiano Faustino <cristiadu@gmail.com>
2014-02-14 10:57:24 +0000

    Fix the layouts from the connect part

--- js/scripts.js
+++ js/scripts.js
@@ -75,3 +75,15 @@ $(document).ready(function(){
 
 
 });
+
+function toggleElement(id)
+{
+    if(document.getElementById(id).style.display == 'none')
+    {
+        document.getElementById(id).style.display = '';
+    }
+    else
+    {
+        document.getElementById(id).style.display = 'none';
+    }
+}
