ec74b96562b59876b2855cea988ebaa2697ad091
Andriana Beltran Romero <a2beltra@ucsd.edu>
2014-02-20 18:13:33 -0800

    meetup startup

--- routes/meetupconfirm.js
+++ routes/meetupconfirm.js
@@ -1,30 +1,4 @@
-var meetup = require ('../meetup.json');
-
 exports.view = function(req, res){
-	res.render('connect-meetupconfirm');
-};
-
-
-
-
-
-
-var dictionary = require('../dictionary.json');
-
-exports.view = function(req, res){
-  res.render('translate');
-};
-
-exports.searchDict = function(req, res){
-
-	var answer = null;
-	console.log("eita");
-	for(var v in dictionary){
-	    if (dictionary[v].name == req.params.word) {
-	        answer = dictionary[v];
-	    }
-	}
-
-  	res.json(answer);
+  res.render('connect-meetupconfirm');
 };
 
